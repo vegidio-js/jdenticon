4b2643d660222c272788c7274c9ce6ebe1cfe696
Vinicius Egidio <me@vinicius.io>
2020-07-25 20:33:19 +0200

    First deploy

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,41 @@
-import * as config from 'config'
+import * as express from 'express'
+import * as jdenticon from 'jdenticon'
 import logger from './logger'
 
-const serverConfig = config.util.toObject(config.get('server'))
+export interface MyRequest extends express.Request {
+    locals: any
+}
 
-const msg = `Server running on ${serverConfig.host}:${serverConfig.port}`
-logger.info(msg)
+const app = express()
+
+app.use((req: MyRequest, res, next) => {
+    if (!req.query.data) {
+        res.status(401).send('Missing query parameter "data"')
+        return
+    }
+
+    req.locals = {
+        data: decodeURIComponent(<string>req.query.data),
+        size: Number(req.query.size ?? 200)
+    }
+
+    next()
+})
+
+app.use('/png', (req: MyRequest, res) => {
+    const buffer = jdenticon.toPng(req.locals.data, req.locals.size)
+    res.contentType('image/png')
+    res.send(buffer)
+})
+
+app.use('/svg', (req: MyRequest, res) => {
+    const buffer = jdenticon.toSvg(req.locals.data, req.locals.size)
+    res.contentType('image/svg+xml')
+    res.send(buffer)
+})
+
+app.use('/', (req, res) => res.send('Jdenticon Generator'))
+
+// Starting server
+const port = process.env.PORT
+app.listen(port, () => logger.info(`🖼  Jdenticon Generator - running on port: ${port}`))
